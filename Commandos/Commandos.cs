b89c72d8e3282c1d0e089523d7c1c20ee2725192
Chaim Shteinberg <a0533197133@gmail.com>
2025-05-27 10:33:24 +0300

    commandos

---
+++ Commandos/Commandos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    public class Commandos
+    {
+        string Name;
+        string CodeName;
+        string[] Weapons = { "Hammer", "chisel", "rope", "bag", "water bottle" };
+        Status status = Status.Standing;
+
+        public Commandos(string name, string codeName)
+        {
+            this.Name = name;
+            this.CodeName = codeName;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("The soldier is walking");
+            this.status = Status.walking;
+        }
+
+        public void Hide()
+        {
+            Console.WriteLine("The soldier is hiding");
+            this.status = Status.hiding;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"The commando 'wiht codeName {this.CodeName} is attacking.");
+        }
+
+        public enum Status
+        {
+            Standing, 
+            walking, 
+            hiding
+        }
+    }
+}
